fb6709486560788d054d6f18c90a6fe268a83f40
Mike Roguez <mike.roguez@gmail.com>
2024-10-09 12:52:21 -0600

    docs: :bookmark: v2024.0.0.22

--- application/src/components/TheFooter.vue
+++ application/src/components/TheFooter.vue
@@ -260,7 +260,7 @@ export default {
                             {{ $t("nav.copyright") }}
                         </p>
                         <p class="my-0  text-sm">
-                            <a class="uline" href="CHANGELOG.md">v2024.0.0.21</a>
+                            <a class="uline" href="CHANGELOG.md">v2024.0.0.22</a>
                         </p>
                     </div>
                 </div>
