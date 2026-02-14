bb3d66a4b1b2fedf0e7ad364b1117ca7c7824229
Mike Roguez <mike.roguez@gmail.com>
2026-02-13 19:01:37 -0600

    adaptacion para gh-pages

--- application/vite.config.js
+++ application/vite.config.js
@@ -6,10 +6,10 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mexihc/',
   build: {
     emptyOutDir: false,
   },
-  base: "",
   plugins: [
     vue(),
     VueI18nPlugin({
