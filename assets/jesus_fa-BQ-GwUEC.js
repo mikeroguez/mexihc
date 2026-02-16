87b6d949a21d8fe78c72a4d899c9983bc1d88928
Mike Roguez <mike.roguez@gmail.com>
2026-02-16 15:25:44 -0600

    Updates

---
+++ assets/jesus_fa-BQ-GwUEC.js
@@ -0,0 +1,1 @@
+const s="/2026/assets/img/people/saiph.jpg",p="/2026/assets/img/people/jesus_fa.jpg";export{s as _,p as a};
---
+++ assets/sanely-CEtPk8Zu.js
@@ -0,0 +1,1 @@
+const s="/2026/assets/img/people/adolfo.jpg",o="/2026/assets/img/people/jose.png",p="/2026/assets/img/people/sanely.jpg";export{s as _,o as a,p as b};
